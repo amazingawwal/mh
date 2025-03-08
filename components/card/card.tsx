0e1572c32772814e51562d6b727a249430a5ba14
amazingawwal <sakeebmawwal@gmail.com>
2025-03-08 17:56:06 +0000

    card component fixed

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -1,58 +1,34 @@
 import React from 'react'
 import Image from 'next/image'
 
-const Card = () => {
+interface Article  {
+  title : String,
+  description: string,
+  src:string,
+  alt:string,
+  id: number,
+  bg: string
+}
 
-    type Article = {
-        title : String,
-        description: string,
-        src:string,
-        alt:string,
-        key: number,
-        bg: string
-    }
-
-    const articles : Article[] = [{
-      title : 'Ramadan',
-      description : "Ramadan is a time of immense blessings, self-improvement, and deepening faith.  🌙✨",
-      src:'/icons8.png',
-      alt:'Ramadan',
-      key: 1,
-      bg:'bg-fuchsia-50'
-    },
-    {
-      title : 'Zakat',
-      description : "Zakat is not just a duty; it is a blessing that purifies wealth, helps the less fortunate, and brings immense rewards.🤲✨",
-      src:'/icons8.png',
-      alt:'zakat',
-      key: 2,
-      bg:'bg-pink-50'
-    }
-  ]
-  return (
-    <div className='pr-2'>{
-        articles.map(article=>(
-      <div key={article.key} className={`flex card lg:card-side mb-2 ${article.bg} shadow-sm`}>
-            <figure className='basis-1/3'>
-              <Image
-                height={20}
-                width={60}
-                // fill
-                // className="object-cover"
-                src={article.src}
-                alt={article.alt} />
-            </figure>
-            <div className=" basis-2/3 p-2 card-body">
-              <h2 className="card-title">{article.title}</h2>
-              <p>{article.description}</p>
-              <div className="card-actions justify-end">
-                <button className="btn text-white hover:text-blue-500 hover:bg-gray-300 bg-blue-400 btn-xs btn-primary">Read</button>
-              </div>
+const Card: React.FC<Article> = ({title, description, src, alt, id, bg}) => {
+  return(
+    <div key={id} className={` flex card lg:card-side mb-2 mr-2 ${bg} shadow-sm`}>
+          <figure className='basis-1/3'>
+            <Image
+              height={20}
+              width={60}
+              src={src}
+              alt={alt} />
+          </figure>
+          <div className=" basis-2/3 p-2 card-body">
+            <h2 className="card-title">{title}</h2>
+            <p>{description}</p>
+            <div className="card-actions justify-end">
+              <button className="btn text-white hover:text-blue-500 hover:bg-gray-300 bg-blue-400 btn-xs btn-primary">Read</button>
             </div>
-      </div>
-        ))
-      }</div>
-  )
+          </div>
+    </div>
+  )  
 }
 
 export default Card
--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -2,11 +2,45 @@ import AutoPlayCarousel from "./carousel";
 import Card from "../card/card";
 
 export default function Hero (){
+    type Article = {
+        title : String,
+        description: string,
+        src:string,
+        alt:string,
+        id: number,
+        bg: string
+    }
+
+    const articles : Article[] = [{
+      title : 'Ramadan',
+      description : "Ramadan is a time of immense blessings, self-improvement, and deepening faith.  🌙✨",
+      src:'/icons8.png',
+      alt:'Ramadan',
+      id: 1,
+      bg:'bg-fuchsia-50'
+    },
+    {
+      title : 'Zakat',
+      description : "Zakat is not just a duty; it is a blessing that purifies wealth, helps the less fortunate, and brings immense rewards.🤲✨",
+      src:'/icons8.png',
+      alt:'zakat',
+      id: 2,
+      bg:'bg-pink-50'
+    }
+  ]
     return(
         <section className="flex p-5">
             <aside className="basis-1/4">
                 <h3 className="">Articles</h3>
-                <Card/>
+                {articles.map(article=>(
+                    <Card title={article.title} 
+                          description={article.description}
+                          src={article.src}
+                          alt={article.alt}
+                          key={article.id}
+                          id={article.id}
+                          bg={article.bg} />
+                ))}
             </aside>
             <aside className="basis-3/4">
                 <AutoPlayCarousel/>
