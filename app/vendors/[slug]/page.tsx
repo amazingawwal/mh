54be84901779ef3051bb3002ac21d77fe0423682
amazingawwal <sakeebmawwal@gmail.com>
2025-03-11 21:45:44 +0000

    try caching

--- app/vendors/[slug]/page.tsx
+++ app/vendors/[slug]/page.tsx
@@ -1,71 +1,28 @@
 
-import Image from "next/image";
-// import { client } from "@/sanity/lib/client";
-// import Image from "next/image";
+
 import { client } from "@/sanity/lib/client";
-import { notFound } from "next/navigation";
 
 import Link from "next/link";
-
-// import { type SanityDocument } from "next-sanity";
+import { type SanityDocument } from "next-sanity";
 import { PortableText } from "next-sanity";
-import { sanityFetch } from "@/sanity/lib/live";
-
-// const POSTS_QUERY = `*[
-//   _type == "post"
-//   && defined(slug.current)
-// ]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`;
-
-
-import imageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
-
 
 const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]`;
+const options = { next: { revalidate: 30 } };
 
-const { projectId, dataset } = client.config();
-const urlFor = (source: SanityImageSource) =>
-  projectId && dataset
-    ? imageUrlBuilder({ projectId, dataset }).image(source)
-    : null;
-
-// const options = { next: { revalidate: 30 } };
-
-export default async function PostPage(
-{
+export default async function PostPage({
   params,
-}: 
-{
-  params: { slug: string };
-}
-) 
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const post = await client.fetch<SanityDocument>(POST_QUERY, await params, options);
 
-{
-//   const post = await client.fetch<SanityDocument>(POST_QUERY, await params, options);
-  const { data: post } = await sanityFetch({ query: POST_QUERY, params: params});
-
-  if (!params?.slug) {
-    notFound();
-  }
-  
-
-  const postImageUrl = post.image
-    ? urlFor(post.image)?.width(150).height(50).url()
-    : null;
 
   return (
     <main className="container mx-auto min-h-screen max-w-3xl p-8 flex flex-col gap-4">
       <Link href="/" className="hover:underline">
         ← Back to posts
       </Link>
-      {postImageUrl && (
-        <Image
-          src={postImageUrl}
-          alt={post.title}
-          width={150}
-          height={50}
-        />
-      )}
+    
       <h1 className="text-4xl font-bold mb-8">{post.title}</h1>
       <div className="prose">
         <p>Published: {new Date(post.publishedAt).toLocaleDateString()}</p>
