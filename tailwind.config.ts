d148ad842d2da3c5b2b7d19aa66bf0bc0b6972e9
amazingawwal <sakeebmawwal@gmail.com>
2025-03-13 22:50:24 +0000

    sanity in progress

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+
+
+
+import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
+import daisyui from "daisyui";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [typography, daisyui], // ✅ Add both plugins
+};
+
+export default config;
